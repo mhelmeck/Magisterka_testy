3c34a526304ab802e3f27af1523c935470d0d8d0
Maciej Hełmecki <mhelmecki@gmail.com>
2021-02-12 21:41:56 +0100

    Control commit

--- learning_512.py
+++ learning_512.py
@@ -12,11 +12,11 @@ epochs = 200
 
 model = build_model()
 
-x_train, y_train = get_images_and_masks(img_width, img_height, 0, 80)
+x_train, y_train = get_images_and_masks(img_width, img_height, 100, 129)
 
 results = model.fit(x_train, y_train, validation_split=0.1, batch_size=16, epochs=epochs, callbacks=get_callbacks("unet_512", 1))
 
-test_images, test_labels = get_images_and_masks(img_width, img_width, 81, 100)
+test_images, test_labels = get_images_and_masks(img_width, img_width, 130, 130)
 
 # Re-evaluate the model
 loss, acc = model.evaluate(test_images, test_labels, verbose=1)
@@ -27,7 +27,7 @@ preds_test = model.predict(test_images, verbose=1)
 preds_test_t = (preds_test > 0.5).astype(np.uint8)
 
 # Perform a sanity check on some random training samples
-ix = 51
+ix = 33
 imshow(test_images[ix])
 plt.show()
 imshow(np.squeeze(test_labels[ix]))
