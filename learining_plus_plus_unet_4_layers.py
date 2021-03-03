8afba9f7c66565039975ec4c08ade9665393f6d5
aleeeksy <aleksy.wolowiec@gmail.com>
2021-03-03 23:01:42 +0100

    make learning processes more similar

--- learining_plus_plus_unet_4_layers.py
+++ learining_plus_plus_unet_4_layers.py
@@ -1,31 +1,29 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from skimage.io import imshow
+from sklearn.metrics import f1_score
 
 from callbacks import get_callbacks
 from models.plus_plus_unet_4_layers import build_model
 from read_data import get_images_and_masks
 from utils import get_save_model_path
 
-img_width = 128
-img_height = 128
+img_width = 512
+img_height = 512
 channel_numbers = 3
-
 epochs = 200
 
+print('Started')
 model_save_path = get_save_model_path('plus_plus_unet_4_layers')
 
 model = build_model(img_width, img_height, channel_numbers)
 print("Model built")
 
-x_train, y_train = get_images_and_masks(img_width, img_height, 100, 180, True)
+x_train, y_train = get_images_and_masks(img_width, img_height, 0, 180, True)
 print("Train data loaded")
 
-results = model.fit(x_train, y_train, validation_split=0.1, batch_size=128, epochs=epochs,
-                    callbacks=get_callbacks(model_save_path))
+results = model.fit(x_train, y_train, validation_split=0.2, batch_size=32, epochs=epochs, callbacks=get_callbacks(model_save_path))
 print("Learning ended")
 
-test_images, test_labels = get_images_and_masks(img_width, img_height, 181, 181, True)
+test_images, test_labels = get_images_and_masks(img_width, img_height, 181, 209, True)
 print("Test data loaded")
 
 loss, acc = model.evaluate(test_images, test_labels, verbose=1)
@@ -34,16 +32,9 @@ print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
 model.load_weights(model_save_path)
 print("Model weights loaded")
 
+print("Prediction started")
 preds_test = model.predict(test_images, verbose=1)
-preds_test_t = (preds_test > 0.3).astype(np.uint8)
+preds_test_t = (preds_test > 0.6).astype(np.uint8)
 
-# Perform a sanity check on some random training samples
-ix = 32
-imshow(test_images[ix])
-plt.show()
-imshow(np.squeeze(test_labels[ix]))
-plt.show()
-imshow(np.squeeze(preds_test_t[ix]))
-plt.show()
-imshow(np.squeeze(preds_test[ix]))
-plt.show()
+f1_score = f1_score(test_labels.flatten().flatten(), preds_test_t.flatten().flatten())
+print('F1 score: %f' % f1_score)
--- learning_unet_6_layers.py
+++ learning_unet_6_layers.py
@@ -6,43 +6,37 @@ from callbacks import get_callbacks
 from models.unet_6_layers import build_model
 from read_data import get_images_and_masks
 from utils import get_save_model_path
+from sklearn.metrics import f1_score
 
 img_width = 512
 img_height = 512
 channel_numbers = 3
 epochs = 200
 
+print('Started')
 model_save_path = get_save_model_path('unet_6_layers')
 
 model = build_model(img_width, img_height, channel_numbers)
-model.summary()
 print("Model built")
 
-x_train, y_train = get_images_and_masks(img_width, img_height, 100, 129)
+x_train, y_train = get_images_and_masks(img_width, img_height, 0, 180, True)
 print("Train data loaded")
 
-results = model.fit(x_train, y_train, validation_split=0.1, batch_size=16, epochs=epochs, callbacks=get_callbacks(model_save_path))
+results = model.fit(x_train, y_train, validation_split=0.2, batch_size=32, epochs=epochs, callbacks=get_callbacks(model_save_path))
 print("Learning ended")
 
-test_images, test_labels = get_images_and_masks(img_width, img_width, 130, 130)
+test_images, test_labels = get_images_and_masks(img_width, img_height, 181, 209, True)
 print("Test data loaded")
 
-# Re-evaluate the model
 loss, acc = model.evaluate(test_images, test_labels, verbose=1)
 print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
 
 model.load_weights(model_save_path)
 print("Model weights loaded")
 
+print("Prediction started")
 preds_test = model.predict(test_images, verbose=1)
+preds_test_t = (preds_test > 0.6).astype(np.uint8)
 
-preds_test_t = (preds_test > 0.5).astype(np.uint8)
-
-# Perform a sanity check on some random training samples
-ix = 33
-imshow(test_images[ix])
-plt.show()
-imshow(np.squeeze(test_labels[ix]))
-plt.show()
-imshow(np.squeeze(preds_test_t[ix]))
-plt.show()
+f1_score = f1_score(test_labels.flatten().flatten(), preds_test_t.flatten().flatten())
+print('F1 score: %f' % f1_score)
