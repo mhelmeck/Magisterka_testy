509351eecf1c22c9727917a4886e411aee5792d1
Kazek <kazek.bystry.strumien@gmail.com>
2021-03-23 22:23:12 +0000

    Refactor code

--- learining_plus_plus_unet_4_layers.py
+++ learining_plus_plus_unet_4_layers.py
@@ -15,26 +15,27 @@ print('Started')
 model_save_path = get_save_model_path('plus_plus_unet_4_layers')
 
 model = build_model(img_width, img_height, channel_numbers)
+model.summary()
 print("Model built")
 
-x_train, y_train = get_images_and_masks(img_width, img_height, 0, 180, True)
-print("Train data loaded")
+# x_train, y_train = get_images_and_masks(img_width, img_height, 0, 180, True)
+# print("Train data loaded")
 
-results = model.fit(x_train, y_train, validation_split=0.2, batch_size=32, epochs=epochs, callbacks=get_callbacks(model_save_path))
-print("Learning ended")
+# results = model.fit(x_train, y_train, validation_split=0.2, batch_size=32, epochs=epochs, callbacks=get_callbacks(model_save_path))
+# print("Learning ended")
 
-test_images, test_labels = get_images_and_masks(img_width, img_height, 181, 209, True)
-print("Test data loaded")
+# test_images, test_labels = get_images_and_masks(img_width, img_height, 181, 209, True)
+# print("Test data loaded")
 
-loss, acc = model.evaluate(test_images, test_labels, verbose=1)
-print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
+# loss, acc = model.evaluate(test_images, test_labels, verbose=1)
+# print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
 
-model.load_weights(model_save_path)
-print("Model weights loaded")
+# model.load_weights(model_save_path)
+# print("Model weights loaded")
 
-print("Prediction started")
-preds_test = model.predict(test_images, verbose=1)
-preds_test_t = (preds_test > 0.6).astype(np.uint8)
+# print("Prediction started")
+# preds_test = model.predict(test_images, verbose=1)
+# preds_test_t = (preds_test > 0.6).astype(np.uint8)
 
-f1_score = f1_score(test_labels.flatten().flatten(), preds_test_t.flatten().flatten())
-print('F1 score: %f' % f1_score)
+# f1_score = f1_score(test_labels.flatten().flatten(), preds_test_t.flatten().flatten())
+# print('F1 score: %f' % f1_score)
--- models/plus_plus_unet_4_layers.py
+++ models/plus_plus_unet_4_layers.py
@@ -1,43 +1,8 @@
 from tensorflow.python.keras.layers import *
 from tensorflow.python.keras.models import Model
 
-# ------------------------------------------------------------------------------------
-
-def conv_block(inputs, filters, pool=True):
-  x = Conv2D(filters, (3, 3), padding="same")(inputs) #kernel_initializer='he_normal'
-  x = BatchNormalization()(x)
-  x = Activation("relu")(x)
-  x = Dropout(0.2)(x)
-
-  x = Conv2D(filters, (3, 3), padding="same")(x) #kernel_initializer='he_normal'
-  x = BatchNormalization()(x)
-  x = Activation("relu")(x)
-  x = Dropout(0.2)(x)
-
-  if pool == True:
-      p = MaxPooling2D((2, 2))(x)
-      return x, p
-  else:
-      return x
-
-# ------------------------------------------------------------------------------------
-
-def conv_block_transpose(inputs, filters, concatenation_list):
-  x = Conv2DTranspose(filters, (2, 2), strides=(2, 2), padding='same')(inputs)
-  x = concatenate(concatenation_list + [x])
-  
-  x = Conv2D(filters, (3, 3), padding="same")(x) #kernel_initializer='he_normal'
-  x = BatchNormalization()(x)
-  x = Activation("relu")(x)
-  
-  x = Conv2D(filters, (3, 3), padding="same")(x) #kernel_initializer='he_normal'
-  x = BatchNormalization()(x)
-  x = Activation("relu")(x)
-  x = Dropout(0.2)(x)
-  
-  return x
-
-# ------------------------------------------------------------------------------------
+from models.building_blocks.conv_block import conv_block
+from models.building_blocks.conv_block_transpose import conv_block_transpose
 
 def build_model(img_width, img_height, channels_number, start_neurons=16):
   inputs = Input((img_width, img_height, channels_number))
