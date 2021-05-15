dfc97c806e66d1da588aee5f3ef5da7f45691540
Maciej Hełmecki <mhelmecki@gmail.com>
2021-05-15 18:05:26 +0200

    Refactor unet

--- models/building_blocks/conv_block_transpose.py
+++ models/building_blocks/conv_block_transpose.py
@@ -12,6 +12,7 @@ def conv_block_transpose(inputs, filters, concatenation_list):
     x = Conv2D(filters, (3, 3), kernel_initializer='he_normal', padding="same")(x)
     x = BatchNormalization()(x)
     x = Activation("relu")(x)
+
     x = Dropout(0.2)(x)
 
     return x
--- models/unet_4_layers.py
+++ models/unet_4_layers.py
@@ -1,77 +1,33 @@
 from tensorflow.python.keras.layers import *
 from tensorflow.python.keras.models import Model
 
+from models.building_blocks.conv_block import conv_block
+from models.building_blocks.conv_block_transpose import conv_block_transpose
+
 
 def build_model(img_width, img_height, channels_number, start_neurons=16):
+    print("[LOG] Start building u-net model. Img_width: ", img_width, ", img_height: ", img_height,
+          ", channels_number: ", channels_number, ", start_neurons: ", start_neurons)
+
     inputs = Input((img_width, img_height, channels_number))
 
+    x00, p0 = conv_block(inputs, start_neurons, pool=True)
+    x10, p1 = conv_block(p0, start_neurons * 2, pool=True)
+    x20, p2 = conv_block(p1, start_neurons * 4, pool=True)
+    x30, p3 = conv_block(p2, start_neurons * 8, pool=True)
 
+    x40 = conv_block(p3, start_neurons * 16, pool=False)
 
-    x10 = BatchNormalization()(p0)
-    x10 = Conv2D(start_neurons * 2, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x10)
-    x10 = Dropout(0.2)(x10)
-    x10 = BatchNormalization()(x10)
-    x10 = Conv2D(start_neurons * 2, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x10)
-    x10 = Dropout(0.2)(x10)
-    p1 = MaxPooling2D((2, 2))(x10)
-
-    x20 = BatchNormalization()(p1)
-    x20 = Conv2D(start_neurons * 4, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x20)
-    x20 = Dropout(0.2)(x20)
-    x20 = BatchNormalization()(x20)
-    x20 = Conv2D(start_neurons * 4, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x20)
-    x20 = Dropout(0.2)(x20)
-    p2 = MaxPooling2D((2, 2))(x20)
-
-    x30 = BatchNormalization()(p2)
-    x30 = Conv2D(start_neurons * 8, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x30)
-    x30 = Dropout(0.2)(x30)
-    x30 = BatchNormalization()(x30)
-    x30 = Conv2D(start_neurons * 8, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x30)
-    x30 = Dropout(0.2)(x30)
-    p3 = MaxPooling2D((2, 2))(x30)
-
-    x40 = BatchNormalization()(p3)
-    x40 = Conv2D(start_neurons * 16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x40)
-    x40 = Dropout(0.2)(x40)
-    x40 = BatchNormalization()(x40)
-    x40 = Conv2D(start_neurons * 16, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x40)
-    x40 = Dropout(0.2)(x40)
-
-    x31 = Conv2DTranspose(start_neurons * 8, kernel_size=(2, 2), strides=(2, 2), padding='same')(x40)
-    x31 = concatenate([x30, x31])
-    x31 = BatchNormalization()(x31)
-    x31 = Conv2D(start_neurons * 8, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x31)
-    x31 = BatchNormalization()(x31)
-    x31 = Conv2D(start_neurons * 8, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x31)
-    x31 = Dropout(0.2)(x31)
-
-    x22 = Conv2DTranspose(start_neurons * 4, kernel_size=(2, 2), strides=(2, 2), padding='same')(x31)
-    x22 = concatenate([x20, x22])
-    x22 = BatchNormalization()(x22)
-    x22 = Conv2D(start_neurons * 4, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x22)
-    x22 = BatchNormalization()(x22)
-    x22 = Conv2D(start_neurons * 4, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x22)
-    x22 = Dropout(0.2)(x22)
-
-    x13 = Conv2DTranspose(start_neurons * 2, kernel_size=(2, 2), strides=(2, 2), padding='same')(x22)
-    x13 = concatenate([x10, x13])
-    x13 = BatchNormalization()(x13)
-    x13 = Conv2D(start_neurons * 2, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x13)
-    x13 = BatchNormalization()(x13)
-    x13 = Conv2D(start_neurons * 2, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x13)
-    x13 = Dropout(0.2)(x13)
-
-    x04 = Conv2DTranspose(start_neurons * 1, kernel_size=(2, 2), strides=(2, 2), padding='same')(x13)
-    x04 = concatenate([x00, x04])
-    x04 = BatchNormalization()(x04)
-    x04 = Conv2D(start_neurons * 1, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x04)
-    x04 = BatchNormalization()(x04)
-    x04 = Conv2D(start_neurons * 1, (3, 3), activation='relu', kernel_initializer='he_normal', padding='same')(x04)
-    x04 = Dropout(0.2)(x04)
+    x31 = conv_block_transpose(x40, start_neurons * 8, [x30])
+    x22 = conv_block_transpose(x31, start_neurons * 4, [x20])
+    x13 = conv_block_transpose(x22, start_neurons * 2, [x10])
+    x04 = conv_block_transpose(x13, start_neurons, [x00])
 
     outputs = Conv2D(1, kernel_size=(1, 1), activation='sigmoid')(x04)
 
     model = Model(inputs=[inputs], outputs=[outputs])
+
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    print("[LOG] U-net model built.")
     return model
